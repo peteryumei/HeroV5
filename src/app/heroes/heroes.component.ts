c7a848fe321a7a93b0c60f50f1e8359dfc335616
Peter Mei <petermei@yahoo.com>
2018-03-04 11:03:03 -0800

    V1

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+//import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+
+  //selectedHero: Hero;
+
+  //onSelect(hero: Hero): void {
+  //  this.selectedHero = hero;
+  //}
+
+  heroes:  Hero[];
+
+  getHeroes(): void {
+    //this.heroes = this.heroService.getHeroes();
+    this.heroService.getHeroes()
+    .subscribe(heroes => this.heroes = heroes = heroes);
+  }
+
+  constructor(private heroService: HeroService) { }
+
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.addHero( { name } as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
+  }
+
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero).subscribe();
+  }
+  
+  ngOnInit() {
+    this.getHeroes();
+  }
+
+}
